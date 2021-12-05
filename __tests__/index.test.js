b0d8ec694ebe473cff0191f249f2c2c61eed9a05
aidos42 <aidos49@yahoo.com>
2021-12-05 09:25:24 +0300

    add many changes in tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,86 +1,138 @@
 import os from 'os';
-import path, { dirname } from 'path';
+import path from 'path';
 import fs from 'fs/promises';
-import { fileURLToPath } from 'url';
 import _ from 'lodash';
+import nock from 'nock';
 import pageLoader from '../src/index.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+nock.disableNetConnect();
 
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFixture = (filename) => fs.readFile(getFixturePath(filename), 'utf-8');
+const getFixturePath = (filename) => path.join(process.cwd(), '__fixtures__', filename);
 
-const dirpath = path.join(os.tmpdir(), 'page-loader-');
+const fixtures = {
+  base: 'https://aidos42.github.io/sample-page/',
+  dir: 'aidos42-github-io-sample-page-index_files',
+  page: {
+    url: 'https://aidos42.github.io/sample-page/index',
+    path: '/index',
+    before: 'before.html',
+    after: 'after.html',
+  },
+  img: {
+    path: '/img/sample1.jpg',
+    name: 'aidos42-github-io-sample-page-img-sample1.jpg',
+    expected: 'sample1.jpg',
+  },
+  css: {
+    path: '/style.css',
+    name: 'aidos42-github-io-sample-page-style.css',
+    expected: 'style.css',
+  },
+  script: {
+    path: '/js/utils.js',
+    name: 'aidos42-github-io-sample-page-js-utils.js',
+    expected: 'utils.js',
+  },
+};
+
 let tempDirpath;
+let page;
+let expectedPage;
+let expectedImg;
+let expectedCSS;
+let expectedScript;
 
 beforeEach(async () => {
   await fs.rmdir(tempDirpath, { recursive: true, force: true }).catch(_.noop);
-  tempDirpath = await fs.mkdtemp(dirpath);
+  tempDirpath = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
+  page = await fs.readFile(getFixturePath(fixtures.page.before), 'utf-8');
+  expectedPage = await fs.readFile(getFixturePath(fixtures.page.after), 'utf-8');
+  expectedImg = await fs.readFile(getFixturePath(fixtures.img.expected), 'utf-8');
+  expectedCSS = await fs.readFile(getFixturePath(fixtures.css.expected), 'utf-8');
+  expectedScript = await fs.readFile(getFixturePath(fixtures.script.expected), 'utf-8');
 });
 
 describe('success', () => {
-  test('expect to return the path to the downloaded file', async () => {
-    const file = 'aidos42-github-io-sample-page-index.html';
-    const filepath = path.join(tempDirpath, file);
+  test('everything is ok', async () => {
+    nock(fixtures.base)
+      .get(fixtures.page.path)
+      .reply(200, page)
+      .get(fixtures.img.path)
+      .reply(200, expectedImg)
+      .get(fixtures.css.path)
+      .reply(200, expectedCSS)
+      .get(fixtures.script.path)
+      .reply(200, expectedScript);
 
-    const expectedAnswer = filepath;
-    const recieved = await pageLoader('https://aidos42.github.io/sample-page/index', tempDirpath);
+    await pageLoader(fixtures.page.url, tempDirpath);
 
-    expect(recieved).toBe(expectedAnswer);
-  });
+    const results = await fs.readdir(tempDirpath);
+    const resultPage = await fs.readFile(path.join(tempDirpath, results[0]), 'utf-8');
+    const resultDir = await fs.access(path.join(tempDirpath, fixtures.dir));
+    const resultImg = await fs.access(path.join(tempDirpath, fixtures.dir, fixtures.img.name));
+    const resultCSS = await fs.access(path.join(tempDirpath, fixtures.dir, fixtures.css.name));
+    const resultScript = await fs.access(path
+      .join(tempDirpath, fixtures.dir, fixtures.script.name));
 
-  test('expect that the file is equal to expected', async () => {
-    const file = 'aidos42-github-io-sample-page-index.html';
-    const filepath = path.join(tempDirpath, file);
-
-    const expectedAnswer = await readFixture('aidos42-github-io-sample-page-index.html');
-
-    await pageLoader('https://aidos42.github.io/sample-page/index', tempDirpath);
-
-    const recieved = await fs.readFile(filepath, 'utf-8');
-
-    expect(recieved).toBe(expectedAnswer);
-  });
-
-  test('expect to find assets in files directory', async () => {
-    const dir = 'aidos42-github-io-sample-page-index_files';
-    const currentDirpath = path.join(tempDirpath, dir);
-
-    await pageLoader('https://aidos42.github.io/sample-page/index', tempDirpath);
-
-    const recieved = await (await fs.readdir(currentDirpath)).sort();
-
-    const expectedAnswer = ['aidos42-github-io-sample-page-img-sample1.jpg',
-      'aidos42-github-io-sample-page-img-sample2.jpg',
-      'aidos42-github-io-sample-page-img-Sample3.jpg',
-      'aidos42-github-io-sample-page-img-sample4.jpg',
-      'aidos42-github-io-sample-page-img-samPLe5.jpg',
-      'aidos42-github-io-sample-page-img-sample6.jpg',
-      'aidos42-github-io-sample-page-img-samplE7.jpg',
-      'aidos42-github-io-sample-page-img-Sample8.jpg',
-      'aidos42-github-io-sample-page-img-SAMPLE9.jpg',
-      'aidos42-github-io-sample-page-img-sample10.jpg',
-      'aidos42-github-io-sample-page-img-sample11.jpg',
-      'aidos42-github-io-sample-page-img-sample12.jpg',
-      'aidos42-github-io-sample-page-img-sample13.jpg',
-      'aidos42-github-io-sample-page-style.css',
-      'aidos42-github-io-sample-page-js-utils.js'].sort();
-
-    expect(recieved).toEqual(expectedAnswer);
+    expect(resultPage).toBe(expectedPage);
+    expect(resultDir).toBeUndefined();
+    expect(resultImg).toBeUndefined();
+    expect(resultCSS).toBeUndefined();
+    expect(resultScript).toBeUndefined();
   });
 });
 
-describe('errors', () => {
-  test('dir doesn\'t exist', async () => {
-    const falseDirpath = '/obviouslyNotExistingDir';
-
-    await expect(pageLoader('https://aidos42.github.io/sample-page/index', falseDirpath)).rejects.toThrow('ENOENT');
+describe('filesysten errors', () => {
+  beforeEach(async () => {
+    nock.cleanAll();
+    nock(fixtures.base)
+      .get(fixtures.page.path)
+      .reply(200, page)
+      .get(fixtures.img.path)
+      .reply(200, expectedImg)
+      .get(fixtures.css.path)
+      .reply(200, expectedCSS)
+      .get(fixtures.script.path)
+      .reply(200, expectedScript);
   });
 
-  test('no permission to access folder', async () => {
-    await fs.chmod(tempDirpath, 0);
+  test('wrong folder', async () => {
+    const wrongTempDirPath = path.join(tempDirpath, '/wrong-folder');
+    const result = await pageLoader(fixtures.page.url, wrongTempDirPath);
 
-    await expect(pageLoader('https://aidos42.github.io/sample-page/index', tempDirpath)).rejects.toThrow('EACCES');
+    await expect(result).rejects.toThrow();
+  });
+
+  test('no access to folder', async () => {
+    await fs.chmod(tempDirpath, 0);
+    const result = await pageLoader(fixtures.page.url, tempDirpath);
+
+    await expect(result).rejects.toThrowError();
+  });
+});
+
+describe('network errors', () => {
+  beforeEach(async () => {
+    nock.cleanAll();
+  });
+
+  test('404', async () => {
+    nock(fixtures.base)
+      .get(fixtures.page.path)
+      .reply(404);
+
+    const result = await pageLoader(fixtures.page.url, tempDirpath);
+
+    await expect(result).rejects.toThrowError();
+  });
+
+  test('500', async () => {
+    nock(fixtures.base)
+      .get(fixtures.page.path)
+      .reply(500);
+
+    const result = await pageLoader(fixtures.page.url, tempDirpath);
+
+    await expect(result).rejects.toThrowError();
   });
 });
