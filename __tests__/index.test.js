6a873c4015dd599296d176b2508fb1801b04d024
aidos42 <aidos49@yahoo.com>
2022-01-06 20:56:20 +0300

    add some fixes after reciew

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -13,101 +13,104 @@ const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const fixtures = {
+const networkErrorsList = [
+  ['404', 404],
+  ['500', 500],
+];
+
+const networkFixtures = {
   base: 'https://ru.hexlet.io/',
   dir: 'ru-hexlet-io-courses_files',
   page: {
     url: 'https://ru.hexlet.io/courses',
+  },
+  error: {
+    path: '/error.html',
+    url: 'https://ru.hexlet.io/error.html',
+  },
+};
+
+const dataFixtures = {
+  page: {
     path: '/courses',
-    before: 'ru-hexlet-io-courses.html',
-    after: '/expected/ru-hexlet-io-courses.html',
+    origin: getFixturePath('ru-hexlet-io-courses.html'),
+    expected: getFixturePath('/expected/ru-hexlet-io-courses.html'),
   },
   img: {
     path: '/assets/professions/nodejs.png',
     name: 'ru-hexlet-io-assets-professions-nodejs.png',
-    expected: '/expected/ru-hexlet-io-courses_files/nodejs.png',
+    expected: getFixturePath('/expected/ru-hexlet-io-courses_files/nodejs.png'),
   },
   css: {
     path: '/assets/application.css',
     name: 'ru-hexlet-io-assets-application.css',
-    expected: '/expected/ru-hexlet-io-courses_files/application.css',
+    expected: getFixturePath('/expected/ru-hexlet-io-courses_files/application.css'),
   },
   link: {
     path: '/courses',
     name: 'ru-hexlet-io-courses.html',
-    expected: '/expected/ru-hexlet-io-courses_files/ru-hexlet-io-courses.html',
+    expected: getFixturePath('/expected/ru-hexlet-io-courses_files/ru-hexlet-io-courses.html'),
   },
   script: {
     path: '/packs/js/runtime.js',
     name: 'ru-hexlet-io-packs-js-runtime.js',
-    expected: '/expected/ru-hexlet-io-courses_files/runtime.js',
-  },
-  error: {
-    server: {
-      path: '/internal-server-error.html',
-      url: 'https://ru.hexlet.io/internal-server-error.html',
-    },
-    notFound: {
-      path: '/not-found.html',
-      url: 'https://ru.hexlet.io/not-found.html',
-    },
-    enoent: {
-      path: '/ENOENT.html',
-      url: 'https://ru.hexlet.io/ENOENT.html',
-    },
+    expected: getFixturePath('/expected/ru-hexlet-io-courses_files/runtime.js'),
   },
 };
 
 let tempDirpath;
-let expectedPage;
-const page = getFixturePath(fixtures.page.before);
-const expectedImg = getFixturePath(fixtures.img.expected);
-const expectedCSS = getFixturePath(fixtures.css.expected);
-const expectedLink = getFixturePath(fixtures.link.expected);
-const expectedScript = getFixturePath(fixtures.script.expected);
 
 beforeEach(async () => {
   await fs.rmdir(tempDirpath, { recursive: true, force: true }).catch(_.noop);
   tempDirpath = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
-  expectedPage = await fs.readFile(getFixturePath(fixtures.page.after), 'utf-8');
 
-  nock(fixtures.base)
-    .get(fixtures.page.path)
-    .replyWithFile(200, page, {
+  nock(networkFixtures.base)
+    .get(dataFixtures.page.path)
+    .replyWithFile(200, dataFixtures.page.origin, {
       'Content-Type': 'text/html',
     })
-    .get(fixtures.img.path)
-    .replyWithFile(200, expectedImg, {
+    .get(dataFixtures.img.path)
+    .replyWithFile(200, dataFixtures.img.expected, {
       'Content-Type': 'image/png',
     })
-    .get(fixtures.css.path)
-    .replyWithFile(200, expectedCSS, {
+    .get(dataFixtures.css.path)
+    .replyWithFile(200, dataFixtures.css.expected, {
       'Content-Type': 'text/css',
     })
-    .get(fixtures.link.path)
-    .replyWithFile(200, expectedLink, {
+    .get(dataFixtures.link.path)
+    .replyWithFile(200, dataFixtures.link.expected, {
       'Content-Type': 'text/html',
     })
-    .get(fixtures.script.path)
-    .replyWithFile(200, expectedScript, {
+    .get(dataFixtures.script.path)
+    .replyWithFile(200, dataFixtures.script.expected, {
       'Content-Type': 'text/javascript',
     });
 });
 
-describe('positive case', () => {
-  test('should work correct', async () => {
-    await pageLoader(fixtures.page.url, tempDirpath);
+describe('positive cases', () => {
+  test('page content should match expected', async () => {
+    await pageLoader(networkFixtures.page.url, tempDirpath);
 
     const results = await fs.readdir(tempDirpath);
     const resultPage = await fs.readFile(path.join(tempDirpath, results[0]), 'utf-8');
-    const resultDir = await fs.access(path.join(tempDirpath, fixtures.dir));
-    const resultImg = await fs.access(path.join(tempDirpath, fixtures.dir, fixtures.img.name));
-    const resultCSS = await fs.access(path.join(tempDirpath, fixtures.dir, fixtures.css.name));
-    const resultLink = await fs.access(path.join(tempDirpath, fixtures.dir, fixtures.link.name));
-    const resultScript = await fs.access(path
-      .join(tempDirpath, fixtures.dir, fixtures.script.name));
+    const expected = await fs.readFile(dataFixtures.page.expected, 'utf-8');
 
-    expect(resultPage).toBe(expectedPage);
+    expect(resultPage).toBe(expected);
+  });
+
+  test('should download assets', async () => {
+    await pageLoader(networkFixtures.page.url, tempDirpath);
+
+    const resultDir = await fs.access(path.join(tempDirpath, networkFixtures.dir));
+    const resultImg = await fs.access(path.join(tempDirpath,
+      networkFixtures.dir, dataFixtures.img.name));
+    const resultCSS = await fs.access(path.join(tempDirpath,
+      networkFixtures.dir, dataFixtures.css.name));
+    const resultLink = await fs.access(path.join(tempDirpath,
+      networkFixtures.dir, dataFixtures.link.name));
+    const resultScript = await fs.access(path
+      .join(tempDirpath, networkFixtures.dir, dataFixtures.script.name));
+
     expect(resultDir).toBeUndefined();
     expect(resultImg).toBeUndefined();
     expect(resultCSS).toBeUndefined();
@@ -121,46 +124,34 @@ describe('negative cases: filesystem errors', () => {
     const wrongTempDirPath = path.join(tempDirpath, '/wrong-folder');
 
     await expect(async () => {
-      await pageLoader(fixtures.page.url, wrongTempDirPath);
+      await pageLoader(networkFixtures.page.url, wrongTempDirPath);
     }).rejects.toThrow(/ENOENT/);
   });
 
   test('should throw error if there is no access to folder', async () => {
     const unaccessableDir = '/root';
     await expect(async () => {
-      await pageLoader(fixtures.page.url, unaccessableDir);
+      await pageLoader(networkFixtures.page.url, unaccessableDir);
     }).rejects.toThrow(/EACCES/);
   });
 });
 
 describe('negative cases: network errors', () => {
-  test('should throw error if timeout', async () => {
-    nock(fixtures.base)
-      .get(fixtures.error.enoent.path)
+  test.each(networkErrorsList)('%s', async (errorText, errorCode) => {
+    nock(networkFixtures.base).get(networkFixtures.error.path).reply(errorCode);
+    const regexp = new RegExp(errorText);
+    await expect(async () => {
+      await pageLoader(networkFixtures.error.url, tempDirpath);
+    }).rejects.toThrow(regexp);
+  });
+
+  test('timeout', async () => {
+    nock(networkFixtures.base)
+      .get(networkFixtures.error.path)
       .replyWithError({ code: 'ETIMEDOUT' });
 
     await expect(async () => {
-      await pageLoader(fixtures.error.enoent.url, tempDirpath);
+      await pageLoader(networkFixtures.error.url, tempDirpath);
     }).rejects.toThrow(/ETIMEDOUT/);
-  });
-
-  test('should throw error if returns 404', async () => {
-    nock(fixtures.base)
-      .get(fixtures.error.notFound.path)
-      .reply(404);
-
-    await expect(async () => {
-      await pageLoader(fixtures.error.notFound.url, tempDirpath);
-    }).rejects.toThrow(/404/);
-  });
-
-  test('should throw error if returns 500', async () => {
-    nock(fixtures.base)
-      .get(fixtures.error.server.path)
-      .reply(500);
-
-    await expect(async () => {
-      await pageLoader(fixtures.error.server.url, tempDirpath);
-    }).rejects.toThrow(/500/);
   });
 });
